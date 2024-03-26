e34a041e99855cd2dab54c15a9cd537f0e31a6a8
sudarshan-bhavikatte <149920240+sudarshan-bhavikatte@users.noreply.github.com>
2024-03-26 15:27:10 +0000

    added time based color gradient and added sun-moon symbols to represent time

--- src/script.js
+++ src/script.js
@@ -1,4 +1,22 @@
 window.onload =  async function() {
+    let currentHour = new Date().getHours();
+    if(currentHour >= 6 && currentHour<=18) {
+        document.querySelector('.day-night').innerHTML =`
+        <img src="sun.png">
+        `;
+    }
+    else if (currentHour >18) {
+        document.querySelector('.day-night').innerHTML = `
+        <img src="moon.png">
+        `;
+        document.querySelector('.card').style.background = 'linear-gradient(to bottom right, grey, black)';
+    }
+    else if (currentHour <6){
+        document.querySelector('.day-night').innerHTML = `
+        <img src="sun-rise.png">
+        `;
+        document.querySelector('.card').style.background = 'linear-gradient(to bottom right,lightgoldenrodyellow,rgb(255, 207, 119))';       
+    }
     let city = 'bangalore';
     const apiKey = 'f6348bbbe9a125abc4e2d84a41d0bfa4';
     let url = `https://api.openweathermap.org/data/2.5/weather?units=metric&q=${city}&appid=${apiKey}`;
